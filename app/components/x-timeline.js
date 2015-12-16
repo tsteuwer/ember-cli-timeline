26d4a1a856759c4e1d353835e1add53900bcf4ff
troy.steuwer <troy.steuwer@bookit.com>
2015-12-15 19:59:20 -0500

    Updating more things.

---
+++ app/components/x-timeline.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-cli-timeline/components/x-timeline';
--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-timeline'
+  name: 'ember-cli-timeline',
+	included: function(app, parentAddon) {
+		var target = parentAddon || app;
+
+		this._super.included(target);
+
+		target.import(target.bowerDirectory + '/TimelineJS3/compiled/js/timeline.js');
+		target.import(target.bowerDirectory + '/TimelineJS3/compiled/css/timeline.css');
+	}
 };
