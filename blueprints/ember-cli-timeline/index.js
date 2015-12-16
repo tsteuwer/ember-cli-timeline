485f688f9ebff525cae3d7cd9a185b356a535d2f
troy.steuwer <troy.steuwer@bookit.com>
2015-12-15 20:26:49 -0500

    trying something new

--- blueprints/ember-cli-timeline/index.js
+++ blueprints/ember-cli-timeline/index.js
@@ -6,7 +6,9 @@ var TIMELINE_JS_VERSION = '3.3.11';
 module.exports = {
   description: 'This plugin injects a TimelineJS component',
 	afterInstall: function(options) {
-		return this.addBowerPackageToProject('https://github.com/NUKnightLab/TimelineJS3@' + TIMELINE_JS_VERSION);
+		this.ui.writeLine('Importing TimlineJS3@' + TIMELINE_JS_VERSION + '...');
+
+		return this.addBowerPackageToProject('https://github.com/NUKnightLab/TimelineJS3.git', TIMELINE_JS_VERSION);
 	},
 	isDevelopingAddon: function() {
 		return true;
